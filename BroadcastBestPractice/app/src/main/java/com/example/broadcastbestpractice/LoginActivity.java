11cdf89d4d0dd1c25048c6210dc106ac81b90b49
AlenPan <a1459578959@gmail.com>
2017-05-01 15:55:29 +0800

    SharedPreferences test

--- BroadcastBestPractice/app/src/main/java/com/example/broadcastbestpractice/LoginActivity.java
+++ BroadcastBestPractice/app/src/main/java/com/example/broadcastbestpractice/LoginActivity.java
@@ -4,7 +4,9 @@ import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -27,6 +29,7 @@ import android.view.inputmethod.EditorInfo;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -42,9 +45,13 @@ import static android.icu.lang.UCharacter.GraphemeClusterBreak.T;
  */
 public class LoginActivity extends BaseActivity {
 
+    private SharedPreferences pref;
+    private SharedPreferences.Editor editor;
+
     private EditText accoutEdit;
     private EditText passwordEdit;
     private Button login;
+    private CheckBox remeberPass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,14 +60,39 @@ public class LoginActivity extends BaseActivity {
 
         accoutEdit = (EditText) findViewById(R.id.accout);
         passwordEdit = (EditText) findViewById(R.id.password);
+        remeberPass = (CheckBox) findViewById(R.id.remeber_pass);
         login = (Button) findViewById(R.id.login);
-        
+
+        pref = PreferenceManager.getDefaultSharedPreferences(this);
+        boolean isRemember = pref.getBoolean("remember_password", false);
+
+        if (isRemember) {
+            // 将账号和密码都设置到文本框中
+            String account = pref.getString("account", "");
+            String password = pref.getString("password", "");
+            accoutEdit.setText(account);
+            passwordEdit.setText(password);
+            remeberPass.setChecked(true);
+        }
+
         login.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
                 String accout = accoutEdit.getText().toString();
                 String password = passwordEdit.getText().toString();
+                Boolean isRemember = remeberPass.isChecked();
+
                 if (accout.equals("admin") && password.equals("123456")) {
+                    editor = pref.edit();
+                    if (isRemember) {
+                        editor.putBoolean("remember_password", isRemember);
+                        editor.putString("account", accout);
+                        editor.putString("password", password);
+                    } else {
+                        editor.clear();
+                    }
+                    editor.apply();
+
                     Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                     startActivity(intent);
                     finish();
